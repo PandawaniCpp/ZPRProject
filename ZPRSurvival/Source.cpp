42e7c7a32f0abf1f8d360037bb6f0632057e1ae4
Kacper Domański <kqpa93@gmail.com>
2014-03-10 23:40:34 +0100

    Minor changes

--- ZPRSurvival/Source.cpp
+++ ZPRSurvival/Source.cpp
@@ -1,20 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include "PerlinNoise.h"
 
 int main() {
+	PerlinNoise map;
+	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+	sf::Image im = map.RenderMap(700, 700 , 100, 0.4, 255, 255, 255);
+	sf::Texture tx;
+	sf::Sprite sprite;
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	srand((int)time(NULL));
+
+
+	tx.create(200, 200);
+	tx.loadFromImage(im);
+	sprite.setTexture(tx);
 
 	while (window.isOpen()) {
 		sf::Event event;
-		while (window.pollEvent(event)) {
+		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(sprite);
 		window.display();
 	}
 
